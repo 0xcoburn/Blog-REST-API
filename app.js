6464ee390d4c67176ed9780e6f450ecb757db3ce
Matt Coburn <mattcoburn24@gmail.com>
2020-05-12 16:50:14 -0700

    login, signup, add post, feed, and pagination

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const graphqlHttp = require("express-graphql");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -49,8 +50,13 @@ app.use((req, res, next) => {
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
+
+app.use(auth);
 
 app.use(
   "/graphql",
